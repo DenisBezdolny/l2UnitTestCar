663b255691c49ae79e2e4c2d4406e3043be9e788
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-03-16 16:18:25 +0300

    Get the car's speed

---
+++ ProjectCar/ProjectCar/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCar
+{
+    public class Car
+    {
+        public int GetRandomSpeed()
+        {
+            var random = new Random();
+            int speed = random.Next(0, 200);
+
+            return (speed);
+        }
+
+
+    }
+}
