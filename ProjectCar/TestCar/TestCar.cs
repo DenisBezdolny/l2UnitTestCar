53a56a3798a80d91c0a6225d25279fa67c2cb097
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-03-20 18:26:38 +0300

    Rename Car unit test Add create-delete new Car-object function with replace arrange level in constructor Add new unit test for using car in different places

---
+++ ProjectCar/TestCar/TestCar.cs
@@ -0,0 +1,55 @@
+using ProjectCar;
+
+namespace TestCar
+{
+    public class TestCar : IDisposable
+    { 
+
+        private Car _car;
+
+        TestCar()
+        {
+            _car = new Car();
+        }
+        public void Dispose()
+        {
+
+        }
+
+        [Fact]
+        public void TestCarSpeedIsTooFastOrNot()
+        {
+            //arrange
+            
+
+            //act
+
+            var car_speed = _car.GetRandomSpeed();
+
+            //array
+
+            if (car_speed > 100)
+            {
+                Assert.Fail("Wow wow, cool down, its too fast");
+            }
+        }
+        [Theory]
+        [InlineData(new object[] { PlaceForUsingCar.City, true })]
+        [InlineData(new object[] { PlaceForUsingCar.Forest, true })]
+        [InlineData(new object[] { PlaceForUsingCar.Desert, false })]
+        [InlineData(new object[] { PlaceForUsingCar.Field, true })]
+        [InlineData(new object[] { PlaceForUsingCar.Ocean, false })]
+        public void CanItBeUsedToDriveInIt(PlaceForUsingCar placeForUsingCar, bool expectedResult)
+        {
+            //arrange in constructor
+
+
+            //act
+            var result = _car.CanItBeUsedInThePlace(placeForUsingCar);
+
+            //assert
+            Assert.Equal(expectedResult, result);
+
+        }
+    }
+}
